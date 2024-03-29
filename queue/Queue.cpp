a89e124e071e7bddda3a2ae161bbeeeb9f4f5626
suraj <surajbiswas182003@gmail.com>
2024-03-29 19:31:13 +0530

    deleted old queue implementation

---
+++ queue/Queue.cpp
@@ -0,0 +1,29 @@
+/*
+    enqueue() -> adds x to the end
+    dequeue() -> remove item from the start
+    size() -> returns no of items in queue
+    getFront() -> returns the front element 
+    getRear() -> returns the last element
+    isFull() -> returns whether the queue is full or not
+    isEmpty() -> returns whether the queue is empty or not
+*/
+
+#include <iostream>
+using namespace std;
+
+class Queue {
+private:
+    
+
+pubilc:
+
+
+};
+
+int main()
+{
+    
+
+
+    return 0;
+}
