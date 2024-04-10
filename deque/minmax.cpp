200602fea9ac79c2c384e2e21e3f9537d051f92b
Suraj Biswas <surajbiswas182003@gmail.com>
2024-04-10 22:06:40 +0530

    added minmax

---
+++ deque/minmax.cpp
@@ -0,0 +1,18 @@
+/*
+    Design a data structure that supprts the following operations O(1) time
+
+    1. insertMin(x) --> inserts a number if it is smallest among others
+    2. insertMax(x) --> inserts a number if it is largest among others
+    3. getMin()     --> returns the smallest number
+    4. getMax()     --> returns the largest number
+    5. extractMax() --> removes the smallest number
+    6. extractMin() --> removes the smallest number
+*/
+
+#include <iostream>
+
+int main () {
+    
+    
+    return 0;
+}
