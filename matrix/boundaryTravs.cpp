f445b865b6233e12aa7b6c6d755d41ddc7cb7f57
Suraj Biswas <surajbiswas182003@gmail.com>
2024-04-14 03:15:57 +0530

    little change

--- matrix/boundaryTravs.cpp
+++ matrix/boundaryTravs.cpp
@@ -26,7 +26,7 @@ void printBoundary(const vector<vector<int>> &arr)
     }
 
     // first col
-    for (int j = r - 2; j >= 1; j--) 
+    for (int j = r - 2; j >= 1; j--)
     {
         cout << arr[j][0] << " ";
     }
@@ -35,12 +35,12 @@ void printBoundary(const vector<vector<int>> &arr)
 
 int main()
 {
-    vector<vector<int>>  arr = {
-                                {1, 2, 3}, 
-                                {4, 5, 6}, 
-                                {7, 8, 9},
-                                {3, 2, 4}
-                                };
+    vector<vector<int>> arr = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {3, 2, 4}
+    };
 
     printBoundary(arr);
 
